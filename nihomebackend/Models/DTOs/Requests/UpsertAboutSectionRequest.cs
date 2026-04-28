95de43ed9e2a34bcd6a4d133c6d0e9df7c8323ad
anhdung2k1 <19521167@gm.uit.edu.vn>
2026-04-28 20:04:19 +0700

    Correct the UI

--- nihomebackend/Models/DTOs/Requests/UpsertAboutSectionRequest.cs
+++ nihomebackend/Models/DTOs/Requests/UpsertAboutSectionRequest.cs
@@ -6,12 +6,12 @@ public class UpsertAboutSectionRequest
 {
     [Required] public string Slug { get; set; } = "";
     public string? ItemsJson { get; set; }
-    [Required] public string Eyebrow { get; set; } = "";
-    [Required] public string TitleA { get; set; } = "";
-    [Required] public string TitleB { get; set; } = "";
-    [Required] public string Paragraph1 { get; set; } = "";
-    [Required] public string Paragraph2 { get; set; } = "";
-    [Required] public string ImageUrl { get; set; } = "";
+    public string Eyebrow { get; set; } = "";
+    public string TitleA { get; set; } = "";
+    public string TitleB { get; set; } = "";
+    public string Paragraph1 { get; set; } = "";
+    public string Paragraph2 { get; set; } = "";
+    public string ImageUrl { get; set; } = "";
     public bool IsActive { get; set; } = true;
     public int SortOrder { get; set; }
 }
