d8476141a77d1b037990797bb5d05f3a53961556
anhdung2k1 <19521167@gm.uit.edu.vn>
2026-08-19 08:04:39 +0700

    Fix customer document test build

--- nihomebackend.tests/Services/CustomerDocumentServiceTests.cs
+++ nihomebackend.tests/Services/CustomerDocumentServiceTests.cs
@@ -1,5 +1,5 @@
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging.Abstractions;
 using Moq;
 using NihomeBackend.Data;
